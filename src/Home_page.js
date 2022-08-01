cb0281704360a7af9d6bea8528e7dbade2f127c8
SHRIMAN <81467966+shrimanm@users.noreply.github.com>
2022-08-01 18:29:35 +0530

    Add files via upload

--- src/Home_page.js
+++ src/Home_page.js
@@ -13,14 +13,13 @@ function  Home_page() {
             <form class="flex flex-col" >
                 <div class="mb-6 pt-3 rounded bg-gray-200">
                     <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Enter vehicle Number</label>
-                    <input type="text" id="email" placeholder="XX-00-XX-0000" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3 uppercase" />
+                    <input type="text" id="vehicle_number" placeholder="XX-00-XX-0000" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3 uppercase" />
                 </div>
                 
                 <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Submit</button>
             </form>
         </section>
     </div>
-
 </div>
 
   )
