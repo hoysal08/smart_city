94b61dae0152d483779cdb6faa89cfefef4518bb
shrimanm <18shrimanmogaveer@gmail.com>
2022-08-05 19:00:11 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
+import { Fragment } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Home_page from './Home_page';
+import Nav_bar from './Nav_bar';
+import Registration_page from './Registration_page';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import { useState } from 'react';
 
 function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+  const connectWalletHandler = () => {
+    if (window.ethereum && window.ethereum.isMetaMask) {
+      window.ethereum
+        .request({ method: 'eth_requestAccounts' })
+        .then((result) => {
+          accountChangedHandler(result[0]);
+          setConnButtonText('Wallet Connected');
+        })
+        .catch((error) => {
+          setErrorMessage(error.message);
+        });
+    } else {
+      console.log('Need to install MetaMask');
+      setErrorMessage('Please install MetaMask browser extension to interact');
+    }
+  };
+
+  const accountChangedHandler = (newAccount) => {
+    setDefaultAccount(newAccount);
+  };
+
   return (
-   <Home_page />
-    );
+    <BrowserRouter>
+      <Container className="mt-3">
+        <Nav_bar
+          connectbutton={connButtonText}
+          defaultaccount={defaultAccount}
+          connectwallet={connectWalletHandler}
+        />
+        <Routes>
+          <Route path="/" element={<Home_page />} />
+          <Route path="/registration" element={<Registration_page />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Home_page.js
+++ src/Home_page.js
@@ -1,28 +1,42 @@
-import React from 'react'
+import React from 'react';
 
-function  Home_page() {
+function Home_page() {
   return (
-    <div class="max-h-screen p-20 bg-gradient-to-r from-cyan-200 via-sky-300 to-blue-400">
-    <div class="bg-white max-w-lg mx-auto p-8 md:p-12 my-14 rounded-lg shadow-2xl">
+    <div class="w-screen h-screen py-20 bg-gradient-to-r from-cyan-200 via-sky-300 to-blue-400 ">
+      <div class="bg-white max-w-lg mx-auto p-8 md:p-12 my-14 rounded-lg shadow-2xl">
         <section>
-            <h3 class="font-bold text-2xl">Welcome to Smart City</h3>
-            <p class="text-gray-600 pt-2">Enter the vehicle information below.</p>
+          <h3 class="font-bold text-2xl">Welcome to Smart City</h3>
+          <p class="text-gray-600 pt-2">Enter the vehicle information below.</p>
         </section>
 
         <section class="mt-10">
-            <form class="flex flex-col" >
-                <div class="mb-6 pt-3 rounded bg-gray-200">
-                    <label class="block text-gray-700 text-sm font-bold mb-2 ml-3" for="email">Enter vehicle Number</label>
-                    <input type="text" id="vehicle_number" placeholder="XX-00-XX-0000" class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3 uppercase" />
-                </div>
-                
-                <button class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200" type="submit">Submit</button>
-            </form>
-        </section>
-    </div>
-</div>
+          <form class="flex flex-col">
+            <div class="mb-6 pt-3 rounded bg-gray-200">
+              <label
+                class="block text-gray-700 text-sm font-bold mb-2 ml-3"
+                for="email"
+              >
+                Enter vehicle Number
+              </label>
+              <input
+                type="text"
+                id="vehicle_number"
+                placeholder="XX-00-XX-0000"
+                class="bg-gray-200 rounded w-full text-gray-700 focus:outline-none border-b-4 border-gray-300 focus:border-purple-600 transition duration-500 px-3 pb-3 uppercase"
+              />
+            </div>
 
-  )
+            <button
+              class="bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 rounded shadow-lg hover:shadow-xl transition duration-200"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
 }
 
-export default Home_page
+export default Home_page;
